5f30a042fb87a581c64df1652cf405b46ee6e48a
Antônio Elvis <102705791+Antonio-Elvis-Dev@users.noreply.github.com>
2025-08-25 17:01:59 -0300

    initial project api-solid-nodejs

---
+++ prisma/migrations/20250825175539_create_relationships/migration.sql
@@ -0,0 +1,24 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `updated_at` on the `check_ins` table. All the data in the column will be lost.
+  - Added the required column `gym_id` to the `check_ins` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `user_id` to the `check_ins` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `password_hash` to the `users` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterTable
+ALTER TABLE "public"."check_ins" DROP COLUMN "updated_at",
+ADD COLUMN     "gym_id" TEXT NOT NULL,
+ADD COLUMN     "user_id" TEXT NOT NULL,
+ADD COLUMN     "validated_at" TIMESTAMP(3);
+
+-- AlterTable
+ALTER TABLE "public"."users" ADD COLUMN     "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+ADD COLUMN     "password_hash" TEXT NOT NULL;
+
+-- AddForeignKey
+ALTER TABLE "public"."check_ins" ADD CONSTRAINT "check_ins_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "public"."users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "public"."check_ins" ADD CONSTRAINT "check_ins_gym_id_fkey" FOREIGN KEY ("gym_id") REFERENCES "public"."gyms"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
